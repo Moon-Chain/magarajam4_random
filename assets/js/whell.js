216e51a4e073f90535315dcac4e87505dbd24e03
moonchain <berkayolce@gmail.com>
2022-03-11 05:32:30 +0300

    index fix

--- assets/js/whell.js
+++
@@ -1,7 +0,0 @@
-const spinButton = document.getElementById('spin');
-const dial = document.querySelector('.dial');
-
-spinButton.addEventListener('click', () => {
-	dial.classList.toggle("spinning");
-	document.querySelector('content-container').classList.toggle('activated');
-});
